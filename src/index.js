5bc512982986c555dd77f95c89b403e44bb35f2e
Andrew <dumpy@dumpyutils.tk>
2022-03-11 21:20:56 +1100

    init

---
+++ src/index.js
@@ -0,0 +1,43 @@
+const os = require("os");
+const path = require("path");
+const fs = require("fs");
+const { exec } = require("child_process");
+var colors = require('colors');
+
+const readline = require("readline").createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+var platform = os.platform();
+var home = os.homedir();
+
+var zoompath = path.join(home, "AppData", "Roaming", "Zoom2");
+
+function main() {
+    console.log(`
+    --------------------------------
+    |      ZOOM ZOOM BYE BYE       |
+    |              V1              |
+    --------------------------------
+    `.rainbow)   
+
+  deleteZoom();
+  
+}
+
+function deleteZoom() {
+  if (platform.toLocaleLowerCase() === "darwin") {
+    console.log("MacOS");
+  } else if (platform.toLocaleLowerCase() === "win32") {
+    fs.rmdir(zoompath, () => {
+      console.log(`
+      ----------------
+      | Zoom Deleted |
+      ----------------
+      `.red);
+    });
+  }
+}
+
+main();
